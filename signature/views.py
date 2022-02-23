6ee3ecdcb70d3f30fe73eb86b8b9540d3ce55d7a
TahminaTania <1611414642@nothsouth.edu>
2022-02-24 02:06:21 +0600

    base

---
+++ signature/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# def index(request):
+#     return HttpResponse("Hello, world.")
+
+# Create your views here.
+
+
+def inputpage(request):
+    return render(request, 'signature/input.html')
+
+
+def outputpage(request):
+    return render (request, 'signature/output.html')
