cd05026c2266aa1cc1aafc8f6c9b3d738b0129da
TahminaTania <1611414642@nothsouth.edu>
2022-02-26 23:24:45 +0600

    multi tab

--- signature/views.py
+++ signature/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import redirect, render,redirect,HttpResponseRedirect
 from django.http import HttpResponse
 from .forms import registration
 from .models import person
+from django.contrib import messages
+
+
 
 
 # def index(request):
@@ -15,18 +19,77 @@ def inputpage(request):
     if request.method == 'POST':
         fm = registration(request.POST)
         if fm.is_valid():
-            fm.save()    
-        NewUser = fm.cleaned_data.get('username')
-
-        
+            cd=fm.cleaned_data
+            shout = person(name = cd['name'], email= cd['email'],Job_Title = cd['Job_Title'],Department = cd['Department'],Company = cd['Company'],Address = cd['Address'],Phone = cd['Phone'])
+            shout.save()
+            fm = registration()
+            return redirect('input')  
+                   
     else:
         fm = registration()
-    people = person.objects.all()
+        
     return render(request, 'signature/input.html', {
-        'form': fm,
-        'pep': people
+        'form': fm
+
     })
      
 
+
+## we need to read a single record , i mean last input 
+#### ------ incomplete work( it showing all data from admin) --https://www.webforefront.com/django/singlemodelrecords.html
+
 def outputpage(request):
-    return render (request, 'signature/output.html')
+    form= registration()
+    if request.method == 'POST':
+        pers = person.objects.all()
+        pers.delete()
+        out = registration(request.POST)
+        if out.is_valid():
+            name=request.cleaned_data.get('name')
+            email=request.POST.get('email')
+            Job_Title=request.POST.get('Job_Title')
+            Department=request.POST.get('Department')
+            Company=request.POST.get('Company')
+            Phone=request.POST.get('Phone')
+            Address=request.POST.get('Address')
+            password=request.POST.get('password')
+            details= person(name=name, email=email, Job_Title =Job_Title,Department=Department,Company=Company,
+            Phone=Phone,Address=Address)
+            details.save()
+    pers= person.objects.all()   
+    return render(request, 'signature/output.html', {
+        'pers':pers
+    })
+ 
+
+           
+           
+    # else:
+    #     fm = registration()
+    # data= person.objects.all()           
+    # return render (request, 'signature/output.html',{
+    #     'form': fm,
+    #     'data': data
+    #     })
+
+
+
+# name=request.POST.get('name')
+#             email=request.POST.get('email')
+#             Job_Title=request.POST.get('Job_Title')
+#             Department=request.POST.get('Department')
+#             Company=request.POST.get('Company')
+#             Phone=request.POST.get('Phone')
+#             Address=request.POST.get('name')
+#             password=request.POST.get('name')
+#  N = fm.cleaned_data['name']
+#             E = fm.cleaned_data['email']
+#             J = fm.cleaned_data['Job_Title']
+#             D = fm.cleaned_data['Department']
+#             C = fm.cleaned_data['Company']
+#             P = fm.cleaned_data['Phone']
+#             A = fm.cleaned_data['Address']
+#             # p = fm.cleaned_data['password']
+#             reg = person(name=N, email=E,Job_Title =J,Department=D,Company=C,
+#             Phone=P,Address=A)
+#             reg.save() 
