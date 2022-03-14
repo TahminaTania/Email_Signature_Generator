eca4e9d6117e17948802435651e97f205852138d
TahminaTania <1611414642@northsouth.edu>
2022-03-14 17:32:50 +0600

    fresh start

--- signature/urls.py
+++ signature/urls.py
@@ -3,12 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('index/', views.index, name='index'),
-    path('edit/<str:pers_name>/',views.Edit, name='edit'),
-    path('signature/reg/', views.RegForm, name="reg"), 
-    path('signature/LogIn/', views.logForm, name="log"),
-    path('home/',views.inputpage, name='input'),
-    path('',views.outputpage.as_view(), name='output'),
+    path('', views.base, name='index'),
+    path('input/', views.input, name='input'),
    
 
 ] 
